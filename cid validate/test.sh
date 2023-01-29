424a63600435db9b6beff8b03230f76143f93d60
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-29 14:12:18 -0300

    fully working installer (also installs elcid), IPFS wrapper, gipfs placeholder

--- cid validate/test.sh
+++
@@ -1,6 +0,0 @@
-valid_cid() {
-  echo $1 | ./elcid d QBU3 >/dev/null 2>&1
-  return $?
-}
-
-valid_cid
--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 unameOutM="$(uname -m)"
 case "${unameOutM}" in
     i286)   flofarch="286";;
@@ -61,3 +65,40 @@ fi
          echo "Testing if ipget works:"
          ipget
 fi
+
+# Install elcid:
+echo "Installing elcid..."
+sudo cp -f include/elcid /usr/bin
+echo "- Turning elcid into an executable..."
+sudo chmod 755 /usr/bin/elcid && sudo chmod +x /usr/bin/elcid
+
+echo "Installing gipfs..." && echo "- Installing gipfs command in /usr/bin..."
+sudo cp -f gipfs /usr/bin
+echo "- Turning gipfs into an executable..."
+sudo chmod +x /usr/bin/gipfs
+
+if [ -f /usr/bin/ipfsoriginal ]; then
+sudo cp -f ipfs /usr/bin
+fi
+#programming tip from an inspired aspirant newbie: if you expect a condition to happen/be used more frequently than others, put it on front/prioritize it!
+if [ ! -f /usr/bin/ipfsoriginal ]; then
+echo "Do you want to install our ipfs replacement? 🧚‍♀️ [Y/n]"
+read wrapipfs
+case $wrapipfs in
+   [nN])
+      echo "Ok."
+      break ;;
+   [yY])
+      sudo cp -rf /usr/bin/ipfs /usr/bin/ipfsoriginal && sudo chmod +x /usr/bin/ipfsoriginal
+      echo "Done."
+      echo "- Installing ipfs wrapper in /usr/bin..."
+      sudo cp -f ipfs /usr/bin
+      echo "- Turning ipfs wrapper into an executable..."
+      sudo chmod 755 /usr/bin/ipfs && sudo chmod +x /usr/bin/ipfs
+      break ;;
+   *)
+      echo "${invalid}" ;;
+esac
+fi
+
+echo "Done! Run 'gipfs' command to use it." && exit
