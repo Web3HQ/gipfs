d4b572cc1a1baa775f78279a0936116f4fb65bd2
Daniella Mesquita <mesquitadaniella@proton.me>
2023-02-02 14:22:08 -0300

    install mimetype for CID files

--- install.sh
+++ install.sh
@@ -104,4 +104,26 @@ case $wrapipfs in
 esac
 fi
 
+echo "Installing mimetypes and their icons..." # this is continuously adding the same entries to /etc/mime.types and have to be fixed
+cat >> /etc/mime.types <<EOF
+text/x-cid			        cid
+EOF
+#-<- should check if line is already added, before re-adding!
+cat > /usr/share/mime/packages/x-cid.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
+  <mime-type type="text/x-cid">
+    <comment>IPFS hash/CID sum file</comment>
+    <generic-icon name="text-x-cid"/>
+    <glob pattern="*.cid"/>
+  </mime-type>
+</mime-info>
+
+EOF
+sudo update-mime-database /usr/share/mime
+
+echo "Installing icons for .cid files..."
+cp include/text-x-cid.svg /usr/share/icons/hicolor/scalable/mimetypes/
+sudo gtk-update-icon-cache /usr/share/icons/gnome/ -f
+
 echo "Done! Run 'gipfs' command to use it." && exit
